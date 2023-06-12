c80ff81bca51674584d8e19ee1e85e3e5d5bc5b5
merveilleuxrenu <136015481+merveilleuxrenu@users.noreply.github.com>
2023-06-12 21:31:56 +0000

    created a json url

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 app = Flask(__name__)
+ITEMS=[{
+  "name":"Galaxy",
+  "desc":"Vast collections of stars"
+  },
+{
+  "name":"planets",
+  "desc":"Celestial bodies in space"
+},
+{
+  "name":"nebulae",
+  "desc":"Cosmic art in celestial canvas"
+}
+       ]
 @app.route("/")
 def helloworld():
-  return render_template("home.html")
+  return render_template("home.html",item=ITEMS)
+@app.route("/api/item")
+def list_items():
+  return jsonify(ITEMS)
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
   
